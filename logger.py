6af6c3a35c3ec6be7a2a124007fc5c82275c8007
StaxxxInc <sersersin2@gmail.com>
2023-04-23 21:20:11 +0300

    work complete

--- logger.py
+++ logger.py
@@ -1,16 +1,42 @@
-import csv
+# Добавление
+def write_data(data):
+    with open('secret documents.csv', 'a', encoding="utf-8") as file:
+        for i in data: file.write(i +' ')
+        print("")
 
-def add_new(contact):
-    try:
-         with open('notebook.csv', 'a') as f:
-            writer = csv.writer(f, delimiter=';')
-            writer.writerows([contact.split(' || ')])
-    except FileNotFoundError:
-         with open('notebook.csv', 'w') as f:
-            writer = csv.writer(f, delimiter=';')
-            writer.writerows([contact.split(' || ')])
-
+# Поиск    
 def get_base():
-    with open('notebook.csv', 'r', encoding='utf-8') as book:
-        return book.read()
-        
+    with open('notebook.csv', 'r', encoding='utf-8') as file:
+        return file.read()
+
+def search_note(base, note):
+    base = base.split('\n')
+    flag = True
+    results = []
+    for i in base:
+        if note in i:
+            flag = False
+            results.append(i)
+    if flag:
+        results.append(f'заметка *{note}* не найдена')
+    return results
+
+# Результаты поиска
+def search_result(result):
+    print('Результаты поиска: ')
+    for i in result:
+        print(i)
+
+
+# Удаление
+def delete(base,s):
+    base = base.split(" ")
+    base.remove(s)
+    return base
+
+# Редактирование
+def update(new_info):
+    new_info_csv =[i.split(" ") for i in new_info]
+    with open('secret documents.csv', 'w', encoding="utf-8") as f:
+        writer = csv.writer(f,delimiter=" ")
+        writer.writerows(new_info_csv)
